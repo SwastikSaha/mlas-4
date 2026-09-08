a52b8faf136df46e73b1b20fde2c26734dd0311f
Prantik Das <prantik.ts@gmail.com>
2026-09-09 00:42:33 +0530

    feat: content

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   cacheComponents: true,
-  serverExternalPackages: ['@napi-rs/canvas'],
+  serverExternalPackages: ["@napi-rs/canvas"],
+  redirects: async () => [
+    {
+      source: "/materials/setup",
+      destination:
+        "https://colab.research.google.com/drive/1Q4AWi5-Z8K9ww0l-P6e_sobUss0ARyMU?usp=sharing",
+      permanent: true,
+    },
+  ],
 };
 
 export default nextConfig;
